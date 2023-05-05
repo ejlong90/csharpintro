292d0584e16f243beffa262a9433cd8803a1c1ec
Eric Long <ejlong21@gmail.com>
2023-05-04 22:23:41 -0400

    may the fourth be with you

---
+++ Pluralsight_practice/Pluralsight_practice/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//C# METHODS ---------------------------------------
+
+// Calculate Wage method
+
+
+
+using Pluralsight_practice;
+
+int amount = 1234;
+int months = 12;
+
+int yearlyWage = Utilities.CalculateYearlyWage(amount, months);
+
+Console.WriteLine($"Yearly wage: {yearlyWage}");
+
+Console.ReadLine();
+
+
+
---
+++ Practice_csharp/Practice_csharp/Program.cs
@@ -0,0 +1,7 @@
+﻿int salaryAmount = 72600;
+string salary = "You make this amount per year: ";
+double postTaxAmount = 1962.25;
+string Paycheck = "You are getting this every two weeks: ";
+
+Console.WriteLine(salary + salaryAmount);
+Console.WriteLine(Paycheck + postTaxAmount);
