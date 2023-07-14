a6f38952a2e9024f013488bc0f925029546d305e
ejlong90 <ejlong21@gmail.com>
2023-07-14 15:35:19 -0400

    7-14js

--- Pomodoro_JS/script.js
+++ Pomodoro_JS/script.js
@@ -13,9 +13,7 @@ window.onload = () => {
   document.getElementById("minutes").innerHTML = workTime;
   document.getElementById("seconds").innerHTML = seconds;
 
-  workTitle.classList.add('active');
-}
+  workTitle.classList.add("active");
+};
 
 // start timer
-
-function start
